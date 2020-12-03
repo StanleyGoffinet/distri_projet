de0f3fb6f382452931c8120422d6efae5215770f
stanleygoffinet <stangoff@hotmail.com>
2020-12-03 18:15:44 +0100

    make circular and unmake circular

--- network.erl
+++ network.erl
@@ -1,37 +1,37 @@
--module(network).
--export([network_list/2,getNeighbors/2,getPID/2,test_getN/0,test_getPID/0,test_network/0,listen/1]).
+-module(net0).
+-export([network_list/2,getNeighbors/2,getPID/2,test_getN/0,test_getPID/0,test_network/0,listen/1,test_makeC/0,test_unmakeC/0]).
 -import(node,[listen/0]).
 % first node
-add(ID,PID,_,[])->
+add(ID,PID,[])->
   [#{id => ID,pid => PID, linked_node_list => [#{id => 1}]}];
 % other node
-add(ID,PID, ID_max,[ #{id := ID_before,pid := PID_before , linked_node_list := List_before} |T ])->
-  if
-    % last node with id = 1 (cause tail recursive implementation)
-    ID == 1 ->
-      [  #{id =>ID,pid => PID , linked_node_list => [#{id=>ID_before}, #{id=>ID_max}]},
-      #{id => ID_before,pid => PID_before, linked_node_list => lists:append([#{id=>ID}],List_before)} |T];
-    % other node
-    ID > 1 ->
-      [#{id =>ID, pid => PID , linked_node_list => [#{id=>ID_before}]},
-      #{id => ID_before,pid => PID_before ,linked_node_list => lists:append([#{id => ID}],List_before)} |T]
-  end.
+add(ID,PID,[ #{id := ID_before,pid := PID_before , linked_node_list := List_before} |T ])->
 
+  [#{id =>ID, pid => PID , linked_node_list => [#{id=>ID_before}]},
+  #{id => ID_before,pid => PID_before ,linked_node_list => lists:append([#{id => ID}],List_before)} |T].
 
-%make_circular(#{id := 1 , linked_node_list := List} |T])->
-%  [ #{id => 1 , linked_node_list => lists:append([#{id =>(lists:length(T)+1)}],List)} | T]
-%  .
+% need filter list with le ID_max in first position and id = 1 in last
+make_circular([#{id := 1, linked_node_list := List}|T])->
+  lists:reverse([ #{id => 1 , linked_node_list => lists:append([#{id =>(length(T)+1)}],List)} | T]).
 
-%make_circular(ID_max, [ #{id := ID_max , linked_node_list := List} |T])->
+make_circular(ID_max, [ #{id := ID_max , linked_node_list := List} |T])->
+  make_circular(lists:reverse([ #{id => ID_max , linked_node_list => lists:append([#{id => 1}],List)} | T])).
 
-%  make_circular(lists:reverse([ #{id => ID_max , linked_node_list => lists:append([#{id => 1}],List)} | T])).
+% undo the cycles on the list
+% need filter list with le ID_max in first position and id = 1 in last
+unmake_circular([#{id := 1}|T])->
+  lists:reverse([ #{id => 1 , linked_node_list =>[#{id => 2}]} | T]).
+
+unmake_circular(1,[#{id := ID_max}|T])->
+  unmake_circular(lists:reverse([ #{id => ID_max , linked_node_list =>[#{id => (ID_max-1)}]} | T])).
+
 
 
 %network_list(0,List_netw)->lists:reverse(List_netw);
 network_list(0,List_netw)->List_netw;
 network_list(Node,List)->
 NodePid = spawn(node,listen,[]),
-network_list(Node-1, add(Node,NodePid, Node+length(List), List)).
+network_list(Node-1, add(Node,NodePid, List)).
 
 
 getPID(_,[])-> "Error, give empty list";
@@ -89,22 +89,20 @@ test_getPID() ->
 test_network() ->
   network_list(5,[])
   .
+test_makeC() ->
+  make_circular(5,[
+  #{id => 5,linked_node_list => [#{id => 4}],pid => 0},
+  #{id => 4,linked_node_list => [#{id => 3},#{id => 5}],pid => 0},
+  #{id => 3,linked_node_list => [#{id => 2},#{id => 4}],pid => 0},
+  #{id => 2,linked_node_list => [#{id => 1},#{id => 3}],pid => 0},
+  #{id => 1,linked_node_list => [#{id => 2}],pid => 0}
+   ]).
 
-%test_makeC() ->
-%  make_circular(5,[
-%  #{id => 5,
-%    linked_node_list => [#{id => 4}],
-%    pid => 0},
-%  #{id => 4,
-%    linked_node_list => [#{id => 3},#{id => 5}],
-%    pid => 0},
-%  #{id => 3,
-%    linked_node_list => [#{id => 2},#{id => 4}],
-%    pid => 0},
-%#{id => 2,
-%   linked_node_list => [#{id => 1},#{id => 3}],
-%   pid => 0},
-%   #{id => 1,
-%    linked_node_list => [#{id => 2}],
-%    pid => 0}
-%   ]).
+ test_unmakeC() ->
+   unmake_circular(1,[
+   #{id => 5,linked_node_list => [#{id => 4},#{id => 1}],pid => 0},
+   #{id => 4,linked_node_list => [#{id => 3},#{id => 5}],pid => 0},
+   #{id => 3,linked_node_list => [#{id => 2},#{id => 4}],pid => 0},
+   #{id => 2,linked_node_list => [#{id => 1},#{id => 3}],pid => 0},
+   #{id => 1,linked_node_list => [#{id => 2},#{id => 5}],pid => 0}
+    ]).
