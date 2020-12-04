72c80dd86ff0158b655c1f14890bc3e290d5392d
stanleygoffinet <stangoff@hotmail.com>
2020-12-04 12:06:31 +0100

    sort reverse circular

--- network.erl
+++ network.erl
@@ -10,20 +10,20 @@ add(ID,PID,[ #{id := ID_before,pid := PID_before , linked_node_list := List_befo
   [#{id =>ID, pid => PID , linked_node_list => [#{id=>ID_before}]},
   #{id => ID_before,pid => PID_before ,linked_node_list => lists:append([#{id => ID}],List_before)} |T].
 
-% need filter list with le ID_max in first position and id = 1 in last
-make_circular([#{id := 1, linked_node_list := List}|T])->
+% need filter list with le ID_max in first position and id = 1 in last with lists:reverse(lists:sort(LIST))
+make_circular2([#{id := 1, linked_node_list := List}|T])->
   lists:reverse([ #{id => 1 , linked_node_list => lists:append([#{id =>(length(T)+1)}],List)} | T]).
 
-make_circular(ID_max, [ #{id := ID_max , linked_node_list := List} |T])->
-  make_circular(lists:reverse([ #{id => ID_max , linked_node_list => lists:append([#{id => 1}],List)} | T])).
+make_circular([ #{id := ID_max , linked_node_list := List} |T])->
+  make_circular2(lists:reverse([ #{id => ID_max , linked_node_list => lists:append([#{id => 1}],List)} | T])).
 
 % undo the cycles on the list
-% need filter list with le ID_max in first position and id = 1 in last
-unmake_circular([#{id := 1}|T])->
+% need filter list with le ID_max in first position and id = 1 in last with lists:reverse(lists:sort(LIST))
+unmake_circular2([#{id := 1}|T])->
   lists:reverse([ #{id => 1 , linked_node_list =>[#{id => 2}]} | T]).
 
-unmake_circular(1,[#{id := ID_max}|T])->
-  unmake_circular(lists:reverse([ #{id => ID_max , linked_node_list =>[#{id => (ID_max-1)}]} | T])).
+unmake_circular([#{id := ID_max}|T])->
+  unmake_circular2(lists:reverse([ #{id => ID_max , linked_node_list =>[#{id => (ID_max-1)}]} | T])).
 
 
 
@@ -88,8 +88,6 @@ listen(LinkedList) ->
   listen(LinkedList).
 
 
-
-
 test_getN() ->
   List = network_list(5,[]),
   [getNeighbors(1,List),getNeighbors(4,List)]
@@ -104,19 +102,19 @@ test_network() ->
   network_list(5,[])
   .
 test_makeC() ->
-  make_circular(5,[
+  make_circular(lists:reverse(lists:sort([
   #{id => 5,linked_node_list => [#{id => 4}],pid => 0},
+  #{id => 1,linked_node_list => [#{id => 2}],pid => 0},
   #{id => 4,linked_node_list => [#{id => 3},#{id => 5}],pid => 0},
   #{id => 3,linked_node_list => [#{id => 2},#{id => 4}],pid => 0},
-  #{id => 2,linked_node_list => [#{id => 1},#{id => 3}],pid => 0},
-  #{id => 1,linked_node_list => [#{id => 2}],pid => 0}
-   ]).
+  #{id => 2,linked_node_list => [#{id => 1},#{id => 3}],pid => 0}
+   ]))).
 
  test_unmakeC() ->
-   unmake_circular(1,[
+   unmake_circular(lists:reverse(lists:sort([
    #{id => 5,linked_node_list => [#{id => 4},#{id => 1}],pid => 0},
+   #{id => 1,linked_node_list => [#{id => 2},#{id => 5}],pid => 0},
    #{id => 4,linked_node_list => [#{id => 3},#{id => 5}],pid => 0},
    #{id => 3,linked_node_list => [#{id => 2},#{id => 4}],pid => 0},
-   #{id => 2,linked_node_list => [#{id => 1},#{id => 3}],pid => 0},
-   #{id => 1,linked_node_list => [#{id => 2},#{id => 5}],pid => 0}
-    ]).
+   #{id => 2,linked_node_list => [#{id => 1},#{id => 3}],pid => 0}
+    ]))).
