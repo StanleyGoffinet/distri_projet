5a279d2633b12fde8505d6ca9309a3835c463c9d
Stanleyffinet <stangoff@hotmail.com>
2020-12-08 19:04:49 +0100

    C fini

--- test.erl
+++ test.erl
@@ -1,7 +1,16 @@
 -module(test).
 -import(network,[listen/1]).
--export([launch/7,test/0]).
+-export([launch/7,healer_rand/0,healer_tail/0,swap_rand/0,swap_tail/0]).
 
+% def : main function to lauch in erlang noshell
+% parameters :
+% H : integer (0-7)
+% S : integer (0-7)
+% C : max lenght of the view
+% Peers : rand or tail for the time of launch
+% Pushpull : boolean true = pushpull, false = push
+% Time : integer, times between cycles in milliseconds
+% N : number of nodes
 launch(H,S,C,Peers,PushPull,Time,N) ->
   ListPid = spawn(network,listen,[[]]),
   ListPid ! {init,floor(N*0.4)},
@@ -21,10 +30,13 @@ launch(H,S,C,Peers,PushPull,Time,N) ->
   cycle(ListPid,1,Time,121),
   ListPid ! {recover,floor(Killed*0.6)},
   cycle(ListPid,1,Time,151).
-  %ListPid ! {launchNodes,7,4,3,true,floor(Time/2),tail},
 
-  %test_time(Time).
-
+% cycles launch
+% parameters :
+% ListPID : spawn the process nodes PID
+% N : Integer
+% Time : Integer in milliseconds
+% Counter : Integer of the last cycle treated
 cycle(ListPid,N,Time,Counter) ->
   if
     N =< 30 ->
@@ -36,9 +48,15 @@ cycle(ListPid,N,Time,Counter) ->
       true
   end.
 
-
-test() ->
-  launch(4,3,7,tail,true,100,10).
-  %file:write_file("log.txt","test\n",[append]).
-  %NewWriting = ["1","2","",""]
-  %file:write_file("/log.txt", io_lib:fwrite("~p\n", [NewWriting]),[append]).
+% healer with rand policy
+healer_rand() ->
+  launch(4,3,7,rand,true,3000,128).
+% healer with tail policy
+healer_tail() ->
+  launch(4,3,7,tail,true,3000,128).
+% swapper with rand policy
+swap_rand() ->
+  launch(3,4,7,rand,true,3000,128).
+% swapper with tail policy
+swap_tail() ->
+  launch(3,4,7,tail,true,3000,128).
